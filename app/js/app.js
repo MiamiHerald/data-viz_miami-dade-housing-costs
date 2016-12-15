60a965452520643e56495d6cfb5c4c1b5d587fe8
Chris Williams <cwilliams@miamiherald.com>
2016-12-15 17:04:45 -0500

    Various tweaks

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,22 @@ var housingApp = angular.module('housingApp', []);
 
 housingApp.controller('HousingFormController', ['$scope',
   function HousingFormController($scope) {
-    $scope.houseType = $('.house:checkbox');
+    $scope.houseType = function() {
+      if ($('.house:checkbox').is(':checked')) {
+        return 'house';
+      } else if ($('.condo:checkbox').is(':checked')) {
+        return 'condo';
+      } else {
+        return undefined;
+      }
+    }
+    $scope.priceType = function() {
+      if ($('.income:checkbox').is(':checked')) {
+        return 'income';
+      } else if ($('.savings:checkbox').is(':checked')) {
+        return 'savings';
+      } else {
+        return undefined;
+      }
+    }
 }]);
